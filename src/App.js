ac1b0f3c9f05b898bb13e827cbe6152b34863766
converse0 <kvasiuksergei2012@yandex.ua>
2021-10-27 10:46:41 +0300

    init 7

--- src/App.js
+++ src/App.js
@@ -43,12 +43,24 @@ function App() {
 
   const onAddToCart = async (obj) => {
     try {
-      if (cartItems.find((item) => Number(item.id) === Number(obj.id))) {
-        setCartItems((prev) => prev.filter((item) => Number(item.id) !== Number(obj.id)));
-        await axios.delete(`https://616d6f856dacbb001794ca43.mockapi.io/cart/${obj.id}`);
+      const findItem = cartItems.find((item) => Number(item.parentId) === Number(obj.id));
+      if (findItem) {
+        setCartItems((prev) => prev.filter((item) => Number(item.parentId) !== Number(obj.id)));
+        await axios.delete(`https://616d6f856dacbb001794ca43.mockapi.io/cart/${findItem.id}`);
       } else {
         setCartItems((prev) => [...prev, obj]);
-        await axios.post('https://616d6f856dacbb001794ca43.mockapi.io/cart', obj);
+        const { data } = await axios.post('https://616d6f856dacbb001794ca43.mockapi.io/cart', obj);
+        setCartItems((prev) =>
+          prev.map((item) => {
+            if (item.parentId === data.parentId) {
+              return {
+                ...item,
+                id: data.id,
+              };
+            }
+            return item;
+          }),
+        );
       }
     } catch (error) {
       alert('Не удалось добавить товар в корзину');
@@ -59,7 +71,7 @@ function App() {
   const onDeleteFromCart = (id) => {
     try {
       axios.delete(`https://616d6f856dacbb001794ca43.mockapi.io/cart/${id}`);
-      setCartItems((prev) => prev.filter((item) => item.id !== id));
+      setCartItems((prev) => prev.filter((item) => Number(item.id) !== Number(id)));
     } catch (error) {
       alert('Ошибка при удалении товара из корзины');
       console.error(error);
@@ -89,7 +101,7 @@ function App() {
   };
 
   const isItemAdded = (id) => {
-    return cartItems.some((obj) => Number(obj.id) === Number(id));
+    return cartItems.some((obj) => Number(obj.parentId) === Number(id));
   };
 
   return (
